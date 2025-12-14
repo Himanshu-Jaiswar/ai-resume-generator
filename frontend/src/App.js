fdcde0075a0e3458ab5232e461b8a6ee7af4a26c
Himanshu-Jaiswar <dilipjaiswar3323@gmail.com>
2025-12-14 10:55:27 +0530

    Change backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/generate-summary", {
+      const response = await fetch("https://resumegenerator1.netlify.app", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
